2e724d01c7ca4a9f018f69f26e0f1af3b8b8ee80
Rifq11 <rifqikamaluddin11@gmail.com>
2025-11-24 06:36:08 +0700

    command

--- lib/utils/api_config.dart
+++ lib/utils/api_config.dart
@@ -1,38 +1,38 @@
 class ApiConfig {
-  static const String baseUrl = 'http://10.67.72.13:8080/api';
+  static const String baseUrl = 'http://192.168.50.1:8080/api'; // use ip to connect as local
 
-  // Auth endpoints
+  // Auth
   static const String register = '/auth/register';
   static const String login = '/auth/login';
   static const String updateProfile = '/auth/profile';
 
-  // Profile endpoints
+  // Profile
   static const String getProfile = '/profile';
   static const String completeProfile = '/profile/complete';
 
-  // Destination endpoints
+  // Destination
   static const String destinations = '/destinations';
   static const String destinationCategories = '/destinations/categories';
   static String destinationById(int id) => '/destinations/$id';
 
-  // Transportation endpoints
+  // Transportation
   static String transportationsByDestination(int id) =>
       '/transportations/destination/$id';
 
-  // Booking endpoints
+  // Booking
   static const String bookings = '/bookings';
   static const String myBookings = '/bookings/my';
   static String bookingById(int id) => '/bookings/$id';
   static String cancelBooking(int id) => '/bookings/$id/cancel';
 
-  // Payment endpoints
+  // Payment
   static const String payments = '/payments';
   static String paymentById(int id) => '/payments/$id';
 
-  // Payment Method endpoints
+  // Payment Method
   static const String paymentMethods = '/payment-methods';
 
-  // Review endpoints
+  // Review
   static const String reviews = '/reviews';
   static String destinationReviews(int id) => '/reviews/destination/$id';
   static String bookingReview(int id) => '/reviews/booking/$id';
